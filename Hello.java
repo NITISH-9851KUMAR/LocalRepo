d57c14ef73b076c542c15e1515984b4259d9472e
NITISH-9835KUMAR <nitish983577@gmail.com>
2024-10-03 19:32:20 +0530

    Add University Name

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ package localRepo;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Print Function");
+        System.out.println("sandip University");
     }
 }
